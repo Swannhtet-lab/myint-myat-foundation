f9c09bd891b3323c267a7ef5176f61b00acd9549
Shweyi113 <ytobio11623@gmail.com>
2026-05-04 15:08:56 +0630

    changing mm

--- src/components/goal/goal.jsx
+++ src/components/goal/goal.jsx
@@ -4,7 +4,10 @@ const Goal = () => {
   return (
     <section id="goal" className="home-section">
       <h1>us goal</h1>
-      
+      <h3>US goal</h3>
+            <h3>US goal</h3>
+
+    <h5></h5>
     </section>
   );
 };
