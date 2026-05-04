e25e4858183087d9c7b1c8a1851e52bcfbf741f7
Shweyi113 <ytobio11623@gmail.com>
2026-05-04 15:06:33 +0630

    update react-router-dom to version 7.14.1 and modify goal component heading

--- src/components/goal/goal.jsx
+++ src/components/goal/goal.jsx
@@ -3,7 +3,7 @@ import './goal.css';
 const Goal = () => {
   return (
     <section id="goal" className="home-section">
-      <h1>Your goal</h1>
+      <h1>us goal</h1>
       
     </section>
   );
